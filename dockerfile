38a5c8d2721294458cdb5c2451bc52fa7842cb95
Aaron J. <arcan_diabdo@hotmail.com>
2025-02-03 15:47:54 -0600

    login funcionando

---
+++ dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.13-slim-buster
+
+ENV PYTHONUNBUFFERED 1
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . $APP_HOME
+
+RUN python3 -m pip install --upgrade pip
+RUN python3 -m pip install -r requirements.txt
+# RUN DOCKER_CLIENT_TIMEOUT=1200
+# RUN COMPOSE_HTTP_TIMEOUT=1200
+RUN python3 -m pip install bcrypt
+
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from database import models
+from database.database import engine, SessionLocal, Base, db_dependency
+from routers import users, customers, inventory, test, transactions
+import time
 
 app = FastAPI()
-
 # Create an instance of Jinja2Templates and point it to the templates directory
 templates = Jinja2Templates(directory="templates")
+models.Base.metadata.create_all(bind=engine)
+app.include_router(users.router)
+app.include_router(inventory.router)
+app.include_router(customers.router)
+app.include_router(transactions.router)
+# app.include_router(test.router)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = (time.perf_counter() - start_time)
+    message = "huevos"
+    print(f"middleware: \t{message}") 
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
